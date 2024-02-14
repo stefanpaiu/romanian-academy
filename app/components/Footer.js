6bf510033f1738e76a3fd95c58242109a7dfb5b9
Stefan <stefan.paiu.0595@gmail.com>
2024-02-14 17:17:11 +0200

    Added basic style, and background. Removed unnecessary crap.

---
+++ app/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p>The Romanian Academy. Copyright &copy; {new Date().getFullYear()}</p>
+    </footer>
+  );
+}
---
+++ app/dashboard/page.js
@@ -0,0 +1,5 @@
+import styles from "@/app/dashboard/dashboard.module.css";
+
+export default function Dashboard() {
+  return <div className={styles.blackBackground}>Hello World!</div>;
+}
